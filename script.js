ea826267aaf65e31b2cb781fac17e0e45780b304
Islana Tavares <islana.polyana@gmail.com>
2024-07-24 16:12:45 -0300

    Calculadora versão 2.0

--- script.js
+++ script.js
@@ -1,36 +1,77 @@
-function calcular() {
-
-    const num1 = parseFloat(document.querySelector("#num1").value)
-    const num2 = parseFloat(document.querySelector("#num2").value)
-
-    const operacao = document.querySelector("#meuSeletor").value
-    let calculo = 0;
-
-    if (operacao == "somar") { calculo = somar(num1, num2) }
-    else if (operacao == "subtrair") { calculo = subtrair(num1, num2) }
-    else if (operacao == "multiplicar") { calculo = multiplicar(num1, num2) }
-    else if (operacao == "dividir") { calculo = dividir(num1, num2) }
-
-    document.querySelector("span").innerText = calculo
+function limparDisplay() {
+    document.querySelector(".calculadora-screen").value = ""
 }
 
-function somar(num1, num2) {
-    return (num1 + num2)
+function addDisplay(value) {
+    if((value !== null) && (value !== undefined)){
+        // addDisplay( localStorage.getItem("memoria"))
+        document.querySelector(".calculadora-screen").value += value
+    }else{
+        addDisplay("")
+    }
+    
 }
 
-function subtrair(num1, num2) {
-    return (num1 - num2)
+function pegarDisplay(){
+    return document.querySelector(".calculadora-screen").value
 }
 
-function multiplicar(num1, num2) {
-    return (num1 * num2)
+
+try {
+    const calculadora = document.querySelector(".calculadora-keys")
+    if (calculadora) {
+
+        calculadora.addEventListener("click", function (event) {
+
+            if ((event.target.value != "=") && (event.target.value != "all-clear") && (event.target.value != "M+") && (event.target.value != "M-") && (event.target.value != "Mr") && (event.target.value != "Mc")) {
+                addDisplay(event.target.value)
+            }
+
+            if (event.target.value == "all-clear") {
+                limparDisplay()
+            }
+            else if (event.target.value == "M+") {
+                localStorage.setItem("memoria", pegarDisplay())
+                limparDisplay()
+            }
+            else if(event.target.value == "M-"){
+               const memoriaSubitraida = localStorage.getItem("memoria") - pegarDisplay()
+               localStorage.setItem("memoria" , memoriaSubitraida)
+               addDisplay(localStorage.getItem("memoria"))
+               limparDisplay()
+            }
+            else if (event.target.value == "Mr") {
+                limparDisplay()
+                addDisplay(localStorage.getItem("memoria"))
+
+              
+
+            }
+            else if (event.target.value == "Mc") {
+                localStorage.removeItem("memoria")
+                limparDisplay()
+            }
+
+        
+
+            else if (event.target.value == "=") {
+                if (document.querySelector(".calculadora-screen").value != "") {
+
+                    const resultado = eval(document.querySelector(".calculadora-screen").value)
+                    document.querySelector(".calculadora-screen").value = resultado
+                }
+            }
+        })
+    }
+
+}
+catch (error) {
+    // console.error("error", error)
+    if (error.name == "TypeError") {
+        console.error("Erro de tipo.", error)
+    }
+
 }
 
-function dividir(num1, num2) {
-    return (num1 / num2)
-}
-function limpar() {
-    document.querySelector("span").innerText = ""
-}
-
+ pegarDisplay()
 
